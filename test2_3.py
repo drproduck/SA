83a67f422cab06898f3a2be09bfb77b6d2889cc7
Khiem Duc Pham <dkp45@en-cc-g2-compute-15.coecis.cornell.edu>
2022-09-07 01:20:33 -0400

    exp, binary covariate and censoring

---
+++ test2_3.py
@@ -0,0 +1,223 @@
+import pdb
+import numpy as np
+import sys
+sys.path.append('..')
+from lifelines import KaplanMeierFitter
+from lifelines import CoxPHFitter
+import matplotlib.pyplot as plt
+import math
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import argparse
+from torch.utils.data import TensorDataset
+from torch.optim import Adam
+from torch.optim.lr_scheduler import StepLR
+
+# test to see if it can learn an exponential model with binary covariate and censoring
+
+np.random.seed(seed=0)
+rate1 = 0.004
+rate2 = 0.002
+rate_cens = 0.002
+n_samples = 5000
+t1 = np.random.exponential(scale=1. / rate1, size=int(n_samples/2))
+t2 = np.random.exponential(scale=1. / rate2, size=int(n_samples/2))
+t = np.concatenate((t1, t2))
+group = np.zeros(n_samples)
+group[int(n_samples/2):] = 1
+
+censtime = np.random.exponential(scale=1. / rate_cens, size=n_samples)
+e = t < censtime
+t[~e] = censtime[~e]
+print(f'{sum(e)}/{n_samples} events observed.')
+
+kmf = KaplanMeierFitter()
+kmf.fit(t[0:int(n_samples/2)], event_observed=e[0:int(n_samples/2)])
+plt.plot(kmf.survival_function_.index.values, kmf.survival_function_.KM_estimate, alpha=0.5, label='km-group-1')
+kmf.fit(t[int(n_samples/2):], event_observed=e[int(n_samples/2):])
+plt.plot(kmf.survival_function_.index.values, kmf.survival_function_.KM_estimate, alpha=0.5, label='km-group-2')
+plt.xticks(np.arange(0, 2000.0001, 200))
+plt.yticks(np.arange(-0.125, 1.0001, 0.125))
+plt.xlim([0,2000])
+plt.ylim([-0.125,1])
+plt.xlabel('Follow-up time (days)')
+plt.ylabel('Proportion surviving')
+plt.title('Simple exponential model with censoring and covariate')
+plt.plot([0, 2000], [0, 0], linestyle='dashed', alpha=0.5)
+# plt.show()
+
+
+# true data generating function
+sample_t = np.linspace(0, 2000, 100)
+curve1 = np.exp(- sample_t * rate1)
+curve2 = np.exp(-sample_t * rate2)
+
+
+plt.plot(sample_t, curve1, alpha=0.5, linestyle='-.', label='ground-truth-group-1')
+plt.plot(sample_t, curve2, alpha=0.5, linestyle='-.', label='ground-truth-group-2')
+
+class Simple(nn.Module):
+    def __init__(self, x_dim):
+        super().__init__()
+        # self.linears = nn.ModuleList()
+        # for i, (in_dim, out_dim) in enumerate(zip(n_dim[:-1], n_dim[1:])):
+        #     self.linears.append(nn.Linear(in_dim, out_dim))
+        #     if i < len(n_dim)-2:
+        #         self.linears.append(nn.ReLU())
+        #     if i < len(n_dim)-2:
+        #         self.linears.append(nn.BatchNorm1d(out_dim))
+
+        self.net = torch.nn.Sequential(
+            nn.Linear(x_dim + 1, 100),
+            # nn.BatchNorm1d(100),
+            nn.ReLU(),
+            nn.Linear(100, 100),
+            # nn.BatchNorm1d(100),
+            nn.ReLU(),
+            # nn.Linear(100, 100),
+            # # nn.BatchNorm1d(100),
+            # nn.ReLU(),
+            nn.Linear(100, 1),
+        )
+
+        # self.net = torch.nn.Sequential(
+        #     nn.Linear(1, 1, bias=True)
+        # )
+
+        # self.ld = torch.nn.Parameter(torch.Tensor([[0.1]]))
+
+    def forward(self, x, t):
+        # t = (t - t_m) / t_std
+        # out = self.ld.expand_as(t)
+        out = self.net(torch.concat((x, t), dim=-1))
+
+        return out
+
+        # return torch.ones_like(t) * math.log(rate)
+
+
+def train_one_epoch(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (X, T, E) in enumerate(train_loader):
+        X, T, E = X.to(device), T.to(device), E.to(device)
+        # data, target = data.to(device), target.to(device)
+        # X: [bs, x_dim], T: [bs, 1], E: [bs, 1]
+        bs = len(X)
+        optimizer.zero_grad()
+        
+        observed_ll = model(X, T) # [bs, 1]
+
+        # sample some times
+        n_time = 1
+        sample_time = torch.rand(bs, n_time).cuda() * T # [bs, n_time]
+        sample_time = sample_time.reshape(-1, 1) # [bs x n_time, 1]
+
+        X = X.unsqueeze(1) # [bs, 1, x_dim]
+        X = X.expand(-1, n_time, -1) # [bs, n_time, x_dim]
+        X = X.reshape(-1, X.shape[-1]) # [bs x n_time, x_dim]
+
+        cum_hz = model(X, sample_time) # [bs x n_time, 1]
+        cum_hz = cum_hz.reshape(bs, -1) # [bs, n_time]
+        cum_hz = torch.exp(cum_hz).mean(-1, keepdim=True) # [bs, 1]
+
+        loss =  -1 * torch.mean(E * observed_ll - cum_hz * T)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(X), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            if args.dry_run:
+                break
+
+def predict_survival_at_t(model, x, t, n_time=1000):
+    model.eval()
+    with torch.no_grad():
+        x = x.reshape(1, -1).cuda() # [1, dim]
+        x = x.expand(n_time, -1) # [n_time, dim]
+        sample_time = torch.linspace(0, t, n_time).cuda().unsqueeze(-1) # [n_time, 1]
+
+        hazard = model(x, sample_time) # [n_time, 1]
+        hazard = torch.exp(hazard)
+        hazard = hazard.squeeze() # [n_time]
+        survival = torch.exp(-1 * hazard.mean() * t)
+
+    return survival.item(), hazard.detach().cpu().numpy()
+
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                    help='input batch size for testing (default: 1000)')
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                    help='number of epochs to train (default: 14)')
+parser.add_argument('--lr', type=float, default=1e-3, metavar='LR',
+                    help='learning rate (default: 1.0)')
+parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                    help='Learning rate step gamma (default: 0.7)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--dry-run', action='store_true', default=False,
+                    help='quickly check a single pass')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--save-model', action='store_true', default=False,
+                    help='For Saving the current Model')
+args = parser.parse_args()
+use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+device = torch.device("cuda" if use_cuda else "cpu")
+
+# normalize by the max of available time.
+t_max = t.max()
+t = t / t_max
+
+# make x_train
+x_train = np.zeros((n_samples, 2))
+x_train[group == 0] = [1, 0]
+x_train[group == 1] = [0, 1]
+
+dataset1 = TensorDataset(torch.from_numpy(x_train).float(), torch.from_numpy(t).unsqueeze(1).float(), torch.from_numpy(e).unsqueeze(1).float())
+train_loader = torch.utils.data.DataLoader(dataset1, batch_size=args.batch_size, shuffle=True)
+
+model = Simple(x_dim=2).to(device)
+print(model)
+optimizer = Adam(model.parameters(), lr=args.lr)
+
+# adding scheduler really helps
+scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+for epoch in range(1, args.epochs + 1):
+    train_one_epoch(args, model, device, train_loader, optimizer, epoch)
+    scheduler.step()
+
+list_survival_rate = []
+for t in np.linspace(0, 2000, 100):
+    survival_rate, hazard_rate = predict_survival_at_t(model, torch.Tensor([[1., 0.]]), t / t_max)
+    # plt.plot(t, survival_rate, 'rx', markersize=5)
+    list_survival_rate.append(survival_rate)
+plt.plot(np.linspace(0, 2000, 100), list_survival_rate, markersize=5, linestyle='--', label='nn-group-1')
+
+list_survival_rate = []
+for t in np.linspace(0, 2000, 100):
+    survival_rate, hazard_rate = predict_survival_at_t(model, torch.Tensor([[0., 1.]]), t / t_max)
+    # plt.plot(t, survival_rate, 'rx', markersize=5)
+    list_survival_rate.append(survival_rate)
+plt.plot(np.linspace(0, 2000, 100), list_survival_rate, markersize=5, linestyle='--', label='nn-group-2')
+
+# x = torch.Tensor([[1.]]).cuda()
+# t = torch.Tensor([[20.]]).cuda()
+# model.eval()
+# y = model(x, t)
+
+print('t_max: ', t_max)
+print(hazard_rate / t_max)
+print(t_max)
+
+plt.legend()
+plt.savefig('test2_3.png')
